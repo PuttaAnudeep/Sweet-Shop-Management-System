81a07b685a4baec252a07acd7fc900252458acae
PuttaAnudeep <anudeeepputta22002@gmail.com>
2025-12-14 11:54:55 +0530

    cleaned configuration

--- backend/jest.config.ts
+++ backend/jest.config.ts
@@ -10,14 +10,7 @@ const config: Config = {
     clearMocks: true,
     transform: {
         '^.+\\.tsx?$': 'ts-jest',
-    },
-    reporters: [
-        'default',
-        ['jest-html-reporter', {
-            pageTitle: 'Test Report',
-            outputPath: './test-report.html',
-        }]
-    ]
+    }
 };
 
 export default config;
